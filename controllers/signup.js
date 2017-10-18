43181d022d28666766c572c258c69593e7607206
jian <jianchengw26@gmail.com>
2017-10-17 21:02:12 -0400

    initial user signup logic

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,6 +1,34 @@
-const user = require('../models/userProfile');
+const User = require('../models/userProfile');
 const express = require('express');
 
 const router = express.Router();
+
+router.post('/',(req,res)=>{
+    if(!req.body.email){
+        res.json({success: false, message: 'You must provide an email'});
+    }else{
+        if(!req.body.username){
+            res.json({success:  false, message: 'You must provide a username'});
+        }else{
+            if(!req.body.password){
+                res.json({success: false, message: 'You must provide a password'});
+            }else{
+                let user = new User({
+                    email: req.body.email.toLowerCase(),
+                    username: req.body.username.toLowerCase(),
+                    password: req.body.password
+                });
+                user.save((err)=>{
+                    if(err){
+                        res.json({success: false, message: 'Can not save to database! '});
+                    }else{
+                        res.json({success: false, message: 'Saved to database! '});
+                    }
+                })
+            }
+        }
+    }
+})
+
 
 module.exports = router;
--- models/userProfile.js
+++ models/userProfile.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 const userProfileSchema = new Schema({
@@ -7,4 +9,14 @@ const userProfileSchema = new Schema({
     password:  { type : String, required:true},
 });
 
+userProfileSchema.pre('save',function(next){
+    if(!this.isModified('password')) return next();
+    bcrypt.hash(this.password,10,(err,hash)=>{
+        if(err) return next(err);
+        this.password = hash;
+        next();
+    })
+})
+
+//export model
 module.exports = mongoose.model('Animal', userProfileSchema);
