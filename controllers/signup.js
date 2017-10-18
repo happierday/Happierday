1adba369c1c7e670e4c23d10bbe2ed293dba5b33
jian <jianchengw26@gmail.com>
2017-10-17 23:58:59 -0400

    more indepth user signup logic with decriptive message

--- controllers/signup.js
+++ controllers/signup.js
@@ -20,10 +20,27 @@ router.post('/',(req,res)=>{
                 });
                 user.save((err)=>{
                     if(err){
-                        res.json({success: false, message: 'Can not save to database! '});
-                    }else{
-                        res.json({success: false, message: 'Saved to database! '});
+                        if(err.code === 11000){
+                            res.json({success: false, message: 'Username or Email already exists!'});
+                        }else{
+                            if(err.errors){
+                                if(err.errors.email){
+                                    res.json({success: false, message: err.errors.email.message});
+                                }else{
+                                    if(err.errors.username){
+                                        res.json({success: false, message: err.errors.username.message});
+                                    }else{
+                                        if(err.errors.password){
+                                            res.json({success: false, message: err.errors.password.message});
+                                        }
+                                    }
+                                }
+                            }else{
+                                res.json({success: false, message: 'can not save user!'});
+                            }
+                        }
                     }
+                    res.json({success: true, message: 'Saved to database! '});
                 })
             }
         }
--- models/userProfile.js
+++ models/userProfile.js
@@ -3,12 +3,111 @@ const bcrypt = require('bcrypt');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
+/*validator user schema
+**validate user input before saving to database
+*/
+
+//email validator
+let  emailLengthChecker = (email) => {
+    if(!email){
+        return false;
+    }else{
+        if(email.length < 5 || email.length > 30){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+let validEmailCheker = (email)  =>  {
+    if(!email){
+        return false;
+    }else{
+        const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regExp.test(email);
+    }
+}
+let emailValidator = [
+    {
+        validator: emailLengthChecker,
+        message: 'Email must be at least 5 characters long but no longer than 30!'
+    },
+    {
+        validator: validEmailCheker,
+        message: 'You must enter a valid email!'
+    }
+]
+
+//username validator
+let  usernameLengthChecker = (username) => {
+    if(!username){
+        return false;
+    }else{
+        if(username.length < 8 || username.length > 20){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+let validUsernameChecker = (username) =>  {
+    if(!username){
+        return false;
+    }else{
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+}
+let usernameValidator = [
+    {
+        validator: usernameLengthChecker,
+        message: 'username must be at least 8 characters long but no longer than 20!'
+    },
+    {
+        validator: validUsernameChecker,
+        message: 'Username should only contain lower,upper case and number!'
+    }
+]
+
+//password validator
+let  passwordLengthChecker = (password) => {
+    if(!password){
+        return false;
+    }else{
+        if(password.length < 8){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+let validPasswordChecker = (password) =>  {
+    if(!password){
+        return false;
+    }else{
+        const regExp = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]+$/);
+        return regExp.test(password);
+    }
+}
+let passwordValidator = [
+    {
+        validator: passwordLengthChecker,
+        message: 'password must be at least 8 characters long!'
+    },
+    {
+        validator: validPasswordChecker,
+        message: 'password should contain at least one lower,upper case and number!'
+    }
+]
+
+//create user schema
 const userProfileSchema = new Schema({
-    email:  { type : String, required:true,  unique:true, lowercase:true},
-    username:  { type : String, required:true,  unique:true, lowercase:true},
-    password:  { type : String, required:true},
+    email:  { type : String, required:true,  unique:true, lowercase:true, validate: emailValidator},
+    username:  { type : String, required:true,  unique:true, lowercase:true, validate: usernameValidator},
+    password:  { type : String, required:true,  validate: passwordValidator},
 });
 
+//before save to mongodb, check whether password is hashed or not
 userProfileSchema.pre('save',function(next){
     if(!this.isModified('password')) return next();
     bcrypt.hash(this.password,10,(err,hash)=>{
@@ -18,5 +117,9 @@ userProfileSchema.pre('save',function(next){
     })
 })
 
+userProfileSchema.methods.validatePassword = (password) => {
+    return bcrypt.compareSync(password,this.password);
+}
+
 //export model
-module.exports = mongoose.model('Animal', userProfileSchema);
+module.exports = mongoose.model('users', userProfileSchema);
