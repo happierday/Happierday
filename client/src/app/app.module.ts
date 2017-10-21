4b631869ddaa1f028537aade4b8f51e2fc64c324
jian <jianchengw26@gmail.com>
2017-10-21 00:28:36 -0400

    dev branch

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
