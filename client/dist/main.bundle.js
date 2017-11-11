9d43c872527345db9cc866dbf9885cc063a82474
jian <jianchengw26@gmail.com>
2017-11-11 01:21:40 -0500

    bugs

--- client/dist/main.bundle.js
+++ client/dist/main.bundle.js
@@ -96,7 +96,7 @@ var AppRoutingModule = (function () {
 AppRoutingModule = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
         imports: [
-            __WEBPACK_IMPORTED_MODULE_0__angular_router__["c" /* RouterModule */].forRoot(appRoutes)
+            __WEBPACK_IMPORTED_MODULE_0__angular_router__["c" /* RouterModule */].forRoot(appRoutes, { useHash: true })
         ],
         exports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_router__["c" /* RouterModule */]
