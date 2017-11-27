3776b030d8810bbf29d1c91074dbcad11cb357ad
jian <jianchengw26@gmail.com>
2017-11-26 21:18:44 -0500

    remove crypto

--- config/config.js
+++ config/config.js
@@ -1,7 +1,4 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
-
 module.exports = {
-    secret: crypto,
     url: 'mongodb://happierday:wu134679@ds135444.mlab.com:35444/happierday',
     email: 'opwu26@gmail.com'
 }
